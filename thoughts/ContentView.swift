e0990b34e88fbcf31b848a13418c1e3be8174937
wmattei <wagner.mattei@gmail.com>
2020-10-04 19:57:47 -0300

    Initial Commit

---
+++ thoughts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  thoughts
+//
+//  Created by Wagner Mattei on 04/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ thoughts/thoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  thoughtsApp.swift
+//  thoughts
+//
+//  Created by Wagner Mattei on 04/10/20.
+//
+
+import SwiftUI
+
+@main
+struct thoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
